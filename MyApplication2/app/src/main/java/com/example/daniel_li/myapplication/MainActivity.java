b205dd383c06c564b0134313c46606dc3fcd9d99
Daniel <li.daniel@berkeley.edu>
2016-01-31 18:34:43 -0800

    spinner works

--- MyApplication2/app/src/main/java/com/example/daniel_li/myapplication/MainActivity.java
+++ MyApplication2/app/src/main/java/com/example/daniel_li/myapplication/MainActivity.java
@@ -1,14 +1,20 @@
+//credit where credit is due, thanks to Eric for helping me with design
+//and for showing me how to use android studio for 3 hours
 package com.example.daniel_li.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+
 
 import java.util.HashMap;
 import java.util.HashSet;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     private HashMap<String, Double> exerciseMap = new HashMap();
     private HashSet<String>  reps = new HashSet();
@@ -20,12 +26,39 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        input = (TextView) findViewById(R.id.exerciseInput);
+        exercise1 = (Spinner) findViewById(R.id.exerciseSpinner1);
+        exercise2 = (Spinner) findViewById(R.id.exerciseSpinner2);
 
-
+        //update spinner
+        exercise1.setOnItemSelectedListener(this);
+        exercise2.setOnItemSelectedListener(this);
+        //add exercises to spinner
+        spinnerGenerate();
 
     }
 
-    private void setHashMap() {
+    public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+        String exerciseType = (String) parent.getItemAtPosition(pos);
+
+    }
+
+    public void onNothingSelected(AdapterView<?> parent) {}
+
+    public void spinnerGenerate() {
+        //http://stackoverflow.com/questions/5241660/how-can-i-add-items-to-a-spinner-in-android
+        ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(this,
+                R.array.exerciseSelection1, android.R.layout.simple_spinner_item);
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        exercise1.setAdapter(adapter1);
+
+        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this,
+                R.array.exerciseSelection2, android.R.layout.simple_spinner_item);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        exercise2.setAdapter(adapter2);
+    }
+
+        private void setHashMap() {
         exerciseMap.put("Pushup", 100.0/350.0);
         exerciseMap.put("Situp", 100.0/200.0);
         exerciseMap.put("Squats", 100.0/225);
